7ae890add76a2eb1fc67491ba76ced605ded8bed
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-11-28 14:05:57 -0600

    Add templates, add controllers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,37 @@
 var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/form', {
+    templateUrl: '/views/templates/form.html',
+    controller: 'FormController'
+  })
+  .when('/superheroes', {
+    templateUrl: '/views/templates/superheroes.html',
+    controller: 'SuperheroController'
+  })
+  // .otherwise({
+  //   redirectTo: 'form'
+  // });
+}]);
+
+app.controller('FormController', ["$http", function($http) {
+console.log('Form controller is running');
+// var self = this;
+// self.warehouses = [];
+//
+// getWarehouses();
+//
+// function getWarehouses() {
+//   //$.ajax
+//   $http.get('/warehouse')
+//     .then(function(response) {
+//       self.warehouses = response.data;
+//     });
+// }
+}]);
+
+app.controller('SuperheroController', ["$http", function($http) {
+console.log('Superhero controller is running');
+
+}]);
